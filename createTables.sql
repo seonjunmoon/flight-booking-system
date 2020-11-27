0c695d4b74d92d8ec6e9600a3847829c8ddac92a
Seonjun Mun <seonjunmun@Seonjuns-MacBook-Pro.local>
2020-11-27 00:02:24 -0800

    createTables.sql: queries to create Users and Reservations tables

--- createTables.sql
+++ createTables.sql
@@ -1,22 +1,18 @@
 CREATE TABLE Users (
     username varchar(20) PRIMARY KEY,
-    hash VARBINARY(100),
-    salt VARBINARY(100),
-    balance int,
-    primary key(username)
+    hash VARBINARY(20),
+    salt VARBINARY(20),
+    balance int
 );
 
-
-CREATE TABLE RESERVATIONS(
-id int,
-fid1 int,
-fid2 int,
-paid int,
-username varchar(20),
-day int,
-price int,
-cancelled int,
-primary key(id),
-foreign key(username) references USERS(username)
-
+CREATE TABLE Reservations (
+    id int PRIMARY KEY,
+    username VARCHAR(20),
+    fid1 int,
+    fid2 int,
+    canceled int,
+    paid int,
+    date int,
+    price int,
+    FOREIGN KEY (username) REFERENCES Users(username)
 );
